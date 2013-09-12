225857f5ca7280cbbf41e4f6083d27ada6d632d7
George Bonner <georgebonnr@gmail.com>
2013-09-12 16:32:12 -0500

    implemented basic username

--- public/chat.js
+++ public/chat.js
@@ -5,22 +5,30 @@ window.onload = function() {
   var field = document.getElementById("field");
   var sendButton = document.getElementById("send");
   var content = document.getElementById("content");
+  var name = document.getElementById("name");
 
   server.on('message', function (data) {
     if (data.message) {
-      messages.push(data.message);
+      messages.push(data);
       var html = '';
       for (var i = 0; i < messages.length; i++) {
-        html += messages[i] + '<br />';
+        html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
+        html += messages[i].message + '<br />';
       }
       content.innerHTML = html;
+      content.scrollTop = content.scrollHeight;
     } else {
-      console.log("Did not get message from server")
+      console.log("Did not get message from server", data)
     }
   });
 
   sendButton.onclick = function() {
-    var text = field.value;
-    server.emit('send', { message: text })
+    if (!name.value) {
+      alert("ENTER NAME")
+    } else {
+      var text = field.value;
+      server.emit('send', { message: text, username: name.value });
+      field.value = "";
+    }
   }
 }
