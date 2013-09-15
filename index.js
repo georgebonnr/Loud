78253b2c6c556b19f2e119b59655091cee4d99ac
George Bonner <georgebonnr@gmail.com>
2013-09-14 21:20:46 -0500

    new functionality and front end markup. needs styles.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var port = 4101;
+var messageList = [];
+var usernames = {}
 
 app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
@@ -14,11 +16,55 @@ app.use(express.static(__dirname + '/public'));
 var io = require('socket.io').listen(app.listen(port));
 console.log('Listening on port ' + port);
 
-io.sockets.on('connection', function (clientEnd) {
-  clientEnd.emit('message', { message: 'VERY NICE TO SEE YOU.' });
+
+
+io.sockets.on('connection', function (client) {
+  // var getU = client.get('username', function(err, name) {
+  //     return name
+  //   });
+
+  // var store = function(data, name, uEvent) {
+  //   var message = {message: data, username: name, uEvent: uEvent}
+  //   messageList.push(message)
+  // };
+
+  messageList.forEach(function (element) {
+    client.emit('message', element)
+  });
+
   console.log('client connected')
-  clientEnd.on('send', function (data) {
-    io.sockets.emit('message', data);
-    console.log(data)
-  })
+
+  client.on('uRequest', function (name, reply) {
+    if (usernames[name]) {
+      reply('already taken')
+    } else {
+      var message = {message: ' joined in.', username: name, uEvent: "join"}
+      client.set('username', name)
+      usernames[name] = name
+      reply('username added')
+      messageList.push(message)
+      io.sockets.emit('message', message)
+      console.log(usernames)
+    }
+  });
+
+  // client.on('disconnect', function(client) {
+  //   var username = getU
+  //   var message = {message: ' is done.', username: username, uEvent: "leave"}
+  //   messageList.push(message)
+  //   client.broadcast.emit('message', message)
+  // });
+
+  client.on('send', function (data) {
+    // var chat = message
+    // console.log('before get ' + message)
+    client.get('username', function(err, name) {
+      var message = {message: data, username: name}
+      messageList.push(message)
+      io.sockets.emit('message', message);
+      messageList.length > 10 && messageList.shift();
+      // NOBODY WANTS TO SHOUT ABOUT YESTERDAY'S NEWS.
+      // console.log(messageList.length)
+    });
+  });
 })
--- public/chat.js
+++ public/chat.js
@@ -1,34 +1,49 @@
-window.onload = function() {
+$(document).ready(function() {
 
   var messages = [];
+  var localUser;
   var server = io.connect('http://localhost:4101');
   var field = document.getElementById("field");
-  var sendButton = document.getElementById("send");
   var content = document.getElementById("content");
   var name = document.getElementById("name");
+  var $chats = $(".chat");
 
-  server.on('message', function (data) {
-    if (data.message) {
-      messages.push(data);
-      var html = '';
-      for (var i = 0; i < messages.length; i++) {
-        html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
-        html += messages[i].message + '<br />';
-      }
-      content.innerHTML = html;
-      content.scrollTop = content.scrollHeight;
-    } else {
-      console.log("Did not get message from server", data)
-    }
+
+
+  // server.on('chats', function (data) {
+  //   $chats.html(" ")
+  //   for (var i=0; i < data.length; i++) {
+  //     var $chatItem = $('<div class="chatItem"></div>')
+  //     console.log(data[i])
+  //     var usernameHTML = data[i].username
+  //     var message = data[i].message
+  //     $chatItem.append(usernameHTML + " " + message)
+  //     $chats.append($chatItem)
+  //   }
+  // });
+
+  server.on('message', function(data) {
+    console.log(data)
+  })
+
+  server.on('newUserNotice', function (data) {
+
+    window.alert(data.username);
   });
 
-  sendButton.onclick = function() {
-    if (!name.value) {
-      alert("ENTER NAME")
-    } else {
-      var text = field.value;
-      server.emit('send', { message: text, username: name.value });
-      field.value = "";
-    }
-  }
-}
+  $('#namesend').on('click', function(e) {
+    e.preventDefault;
+    server.emit('uRequest', name.value, function(data) {
+      console.log(data)
+    })
+  })
+
+  $('#send').on('click', function(e) {
+    e.preventDefault;
+    // server.emit('newUser', { message: name.value });
+    // var text = $('.field').value().toUpperCase();
+    var text = 'blah blah blah'
+    server.emit('send', text);
+    field.value = "";
+  });
+});
